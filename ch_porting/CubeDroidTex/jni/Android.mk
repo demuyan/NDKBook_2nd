51c38ac59648fdad8cc93197616171b837e62ec5
N.Demura <demuyan@gmail.com>
2012-08-03 15:36:37 +0900

    テクスチャー崩れを修正

--- ch_porting/CubeDroidTex/jni/Android.mk
+++ ch_porting/CubeDroidTex/jni/Android.mk
@@ -1,17 +1,24 @@
+#/////begin libpng_samplecode_02
 LOCAL_PATH := $(call my-dir)
 
+# libpngをAndroidで利用できるように
+# prebuiltしたライブラリは直接リンクできない
 include $(CLEAR_VARS)
 LOCAL_MODULE := libpng15-prebuilt
 LOCAL_SRC_FILES := lib/libpng15.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
+# アプリにリンクするモジュールを生成する
 include $(CLEAR_VARS)
 LOCAL_MODULE    := cubedroid
-LOCAL_SRC_FILES := main.c libpng_android.c
-LOCAL_LDLIBS    := -llog -lz -landroid -lEGL -lGLESv1_CM 
-LOCAL_STATIC_LIBRARIES := android_native_app_glue libpng15-prebuilt
-
+LOCAL_SRC_FILES := main.c libpng_android.c glu.c
+LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM \
+                   -lz  # libpngはlibzをリンクする必要がある
+LOCAL_STATIC_LIBRARIES := android_native_app_glue  # NativeActivityGlue
+LOCAL_STATIC_LIBRARIES += libpng15-prebuilt         # libpng15
 include $(BUILD_SHARED_LIBRARY)
 
+# NativeActivityGlueのモジュール
 $(call import-module,android/native_app_glue)
+#/////end
