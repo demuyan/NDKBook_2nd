6e796373e980a5dded0b765c2cba8760c56093e7
Narikazu DEMURA <demuyan@gmail.com>
2012-08-10 14:45:48 +0900

    リファクタリングした

--- ch03/GLNativeActivity/jni/Android.mk
+++ ch03/GLNativeActivity/jni/Android.mk
@@ -1,13 +1,19 @@
+#/////begin android_mk_samplecode
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
 
+#生成するモジュール名
 LOCAL_MODULE    := native-activity
+#ソースコード
 LOCAL_SRC_FILES := main.c
-LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM
-LOCAL_STATIC_LIBRARIES := android_native_app_glue
-
+#リンクするライブラリ
+LOCAL_LDLIBS    := -llog \
+                   -landroid \        #=====(1)
+                   -lEGL -lGLESv1_CM
+#静的リンクする外部モジュール
+LOCAL_STATIC_LIBRARIES := android_native_app_glue #=====(2)
+#共有モジュールとしてビルドする
 include $(BUILD_SHARED_LIBRARY)
-
-$(call import-module,android/native_app_glue)
-
+#外部モジュールのパス指定
+$(call import-module,android/native_app_glue) #=====(3)
+#/////end
--- ch06/AccessAssets/jni/main.c
+++ ch06/AccessAssets/jni/main.c
@@ -14,36 +14,37 @@ jobject getAssetsFilelistInDir(JNIEnv* env, jclass thiz, jobject assetManager,
 
   // AssetManagerを取得
   AAssetManager* mgr = AAssetManager_fromJava(env, assetManager);  /////-----(1)
-  // ディレクトリ名をchar型に変換して取得
-  const jbyte *dname = (*env)->GetStringUTFChars(env, dirName, NULL);
-  // ディレクトリを開く
-  AAssetDir* assetDir = AAssetManager_openDir(mgr, dname); /////-----(2)
-  // UTF文字列を開放する
-  (*env)->ReleaseStringUTFChars(env, dirName, dname);
+  // 指定したディレクトリを開く
+  const jbyte *dname = (*env)->GetStringUTFChars(env, dirName, NULL); /////-----(2) ここから
+  AAssetDir* assetDir = AAssetManager_openDir(mgr, dname); 
+  (*env)->ReleaseStringUTFChars(env, dirName, dname); /////-----(2)ここまで
+
   // 先頭のポインタに移動する
   AAssetDir_rewind(assetDir);  /////-----(3)
-  // ファイルの数をカウントする
+
+  // ファイル名を保存する領域を確保
   char* filename;
   int count = 0;
-  while ((filename = AAssetDir_getNextFileName(assetDir)) != NULL) { /////-----(4)
+  while ((filename = AAssetDir_getNextFileName(assetDir)) != NULL) { 
     count++;
   };
-  // Stringの配列を新規作成する
   jobjectArray objary = (jobjectArray) (*env)->NewObjectArray(
       env, count, (*env)->FindClass(env, "java/lang/String"),
       (*env)->NewStringUTF(env, ""));
+
   // 先頭のポインタに移動する
   AAssetDir_rewind(assetDir);
   // String配列にファイル名を格納する
   int i = 0;
-  while ((filename = AAssetDir_getNextFileName(assetDir)) != NULL) {
+  while ((filename = AAssetDir_getNextFileName(assetDir)) != NULL) {  /////-----(4) ここから
     (*env)->SetObjectArrayElement(env, objary, i,
                                   (*env)->NewStringUTF(env, filename));
     i++;
-  }
+  } /////-----(4) ここまで
+
   // ディレクトリを閉じる
   AAssetDir_close(assetDir); /////-----(5)
-  // String配列を返す
+
   return objary;
 }
 /////end
@@ -53,27 +54,32 @@ jobject getAssetsReadTextfile(JNIEnv* env, jclass thiz, jobject assetManager,
                           jstring filename) {
 
   // AssetManagerを取得
-  AAssetManager* mgr = AAssetManager_fromJava(env, assetManager); /////-----(1)
-  // ファイル名をchar型に変換して取得
-  const jbyte *fname = (*env)->GetStringUTFChars(env, filename, NULL);
-  // ファイルをオープンする
-  AAsset* assetFile = AAssetManager_open(mgr, fname, AASSET_MODE_RANDOM);  /////-----(2)
-  // 文字列バッファを開放する
-  (*env)->ReleaseStringUTFChars(env, filename, fname);
-  // 長さを取得
-  int length = AAsset_getLength(assetFile); /////-----(3)
-  // 読み込みバッファを取得
+  AAssetManager* mgr = AAssetManager_fromJava(env, assetManager); 
+
+  // 指定したファイルをオープンする
+  const jbyte *fname = (*env)->GetStringUTFChars(env, filename, NULL); /////-----(1) ここから
+  AAsset* assetFile = AAssetManager_open(mgr, fname, AASSET_MODE_RANDOM);  
+  (*env)->ReleaseStringUTFChars(env, filename, fname); /////-----(1) ここまで
+
+  // ファイルサイズを取得
+  int length = AAsset_getLength(assetFile); /////-----(2)
+
+  // バッファを取得
   u_char *buf = (u_char*) malloc(length + 1);
   bzero(buf, length + 1);
+
   // ファイルを読み込み
-  AAsset_read(assetFile, buf, length); /////-----(4)
+  AAsset_read(assetFile, buf, length); /////-----(3)
+
   // UTF-8に変換
   jobject jstr = (*env)->NewStringUTF(env, buf);
-  // 読み込みバッファを開放
+
+  // バッファを解放
   free(buf);
+
   // ファイルを閉じる
-  AAsset_close(assetFile);  /////-----(5)
-  // 文字列を返す
+  AAsset_close(assetFile);  /////-----(4)
+
   return jstr;
 }
 /////end
